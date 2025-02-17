0fdf88eba50b0d91438d8654d8cce9d4eefd12fe
Sayantani Dasgupta <sayantani.dasgupta@ayata.com>
2025-02-17 08:43:39 +0000

    add blurring methoids

--- src/unstructured_data_api/image/noise_reduction.py
+++ src/unstructured_data_api/image/noise_reduction.py
@@ -1,9 +1,44 @@
+import numpy as np
+import cv2
 
-def gaussian_blur():
-    pass
+def average_blur(img, kernel_size=(5, 5)):
+    """It replaces each pixel's value with the average of its neighboring pixel values within the kernel size"""
+    
+    if not isinstance(img, np.ndarray):
+        img = np.array(img)
+        
+    return cv2.blur(img, kernel_size) 
 
-def median_blur():
-    pass
+def gaussian_blur(img, kernel_size=(5, 5)):
+    """It uses a Gaussian function (bell-shaped curve) to calculate the weights for the neighboring pixels.Pixels closer to the center have higher weights, resulting in a more natural blur"""
+    
+    if not isinstance(img, np.ndarray):
+        img = np.array(img)
+        
+    return cv2.GaussianBlur(img, kernel_size, 0) 
 
-def bilateral_filter():
-    pass
+def median_blur(img, kernel_size=5):
+    """Replaces each pixel's value with the median value of its neighbors"""
+
+    if not isinstance(img, np.ndarray):
+        img = np.array(img)
+    
+    return cv2.medianBlur(img, kernel_size) 
+
+def bilateral_filter(img):
+    """It's an edge-preserving blurring technique. It applies a Gaussian filter in both the spatial domain and the intensity domain. This means that it considers both the distance between pixels and their color difference, preserving sharp edges while smoothing the rest of the image"""
+    
+    if not isinstance(img, np.ndarray):
+        img = np.array(img)
+    
+    return cv2.bilateralFilter(img, 9, 75, 75)
+
+def motion_blur(img):
+    if not isinstance(img, np.ndarray):
+        img = np.array(img)
+        
+    kernel = np.zeros((11, 11))
+    kernel[:, 5] = np.ones(11)
+    kernel /= 11
+    return cv2.filter2D(img, -1, kernel)
+
--- src/unstructured_data_api/image/thresholding.py
+++ src/unstructured_data_api/image/thresholding.py
@@ -1,43 +1,65 @@
 import numpy as np
 import cv2
 
-def thresh_binary(im):
+def thresh_global(im, threshold=127):
+    """Image has to be in gray scale"""
+    
     if not isinstance(im, np.ndarray):
         im = np.array(im)
         
-    return cv2.threshold(im, 127, 255, cv2.THRESH_BINARY)
+    return cv2.threshold(im, threshold, 255, cv2.THRESH_BINARY)
 
-def thresh_binaryInv(im):
+def thresh_binaryInv(im, threshold=127):
+    """Image has to be in gray scale"""
+    
     if not isinstance(im, np.ndarray):
         im = np.array(im)
         
-    return cv2.threshold(im, 127, 255, cv2.THRESH_BINARY_INV)
+    return cv2.threshold(im, threshold, 255, cv2.THRESH_BINARY_INV)
 
-def thresh_trunc(im):
+def thresh_trunc(im, threshold=127):
+    """Image has to be in gray scale"""
+    
     if not isinstance(im, np.ndarray):
         im = np.array(im)
-    return cv2.threshold(im, 127, 255, cv2.THRESH_TRUNC)
+    return cv2.threshold(im, threshold, 255, cv2.THRESH_TRUNC)
 
-def thresh_to_zero(im):
+def thresh_to_zero(im, threshold=127):
+    """Image has to be in gray scale"""
+    
     if not isinstance(im, np.ndarray):
         im = np.array(im)
         
-    return cv2.threshold(im, 127, 255, cv2.THRESH_TOZERO)
+    return cv2.threshold(im, threshold, 255, cv2.THRESH_TOZERO)
 
-def thresh_to_zero_inv(im):
+def thresh_to_zero_inv(im, threshold=127):
+    """Image has to be in gray scale"""
+    
     if not isinstance(im, np.ndarray):
         im = np.array(im)
         
-    return cv2.threshold(im, 127, 255, cv2.THRESH_TOZERO_INV)
+    return cv2.threshold(im, threshold, 255, cv2.THRESH_TOZERO_INV)
 
 def thresh_adaptive_mean(im):
+    """Image has to be in gray scale"""
+    
     if not isinstance(im, np.ndarray):
         im = np.array(im)
         
     return cv2.adaptiveThreshold(im, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
 
 def thresh_adaptive_gaussian(im):
+    """Image has to be in gray scale"""
+    
     if not isinstance(im, np.ndarray):
         im = np.array(im)
         
     return cv2.adaptiveThreshold(im, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+
+def thresh_otsu(im):
+    """Image has to be in gray scale"""
+    
+    if not isinstance(im, np.ndarray):
+        im = np.array(im)
+        
+    return cv2.threshold(im, 0,255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
